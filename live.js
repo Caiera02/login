6fbb9f79a3c842e5f2ef84c77ec1f36f3846a794
Caiera02 <caiocezar2807@gmail.com>
2024-10-21 17:24:15 -0300

    Atualição

---
+++ live.js
@@ -0,0 +1,5 @@
+// // Função para redirecionar após 5 segundos
+// setTimeout(function () {
+//     // Redireciona para a página inicial
+//     window.location.href = "index.html"; // Altere "index.html" para a URL da sua página inicial
+// }, 5000); // 5000 milissegundos = 5 segundos
--- script.js
+++ script.js
@@ -40,4 +40,10 @@ form.addEventListener('submit', function(event) {
        errorMsg.textContent = "Nome de usuário ou senha incorretos.";
         mensagem.style.color = "red";
     }
+
+    // Função para redirecionar após 5 segundos
+    setTimeout(function() {
+        // Redireciona para a página inicial
+        window.location.href = "index.html"; // Altere "index.html" para a URL da sua página inicial
+    }, 5000); // 5000 milissegundos = 5 segundos
 });
